1be0087b943d08eb1b8784cf7d153c7dc9835a21
Abdumutal <aka.orihero@gmail.com>
2024-05-10 00:53:18 +0500

    added carousel

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,78 @@
-import React, { Component } from 'react';
-import Container from '@/components/Container';
 import Card from '@/components/Card';
-import RN from '@/components/RN';
+import Carousel from '@/components/Carousel';
+import Container from '@/components/Container';
 import CardHorizantalFilter from '@/components/Filters/CardHorizantalFilter';
+import RN from '@/components/RN';
+import { MockUserImagePng } from '@/shared/assets/mock/images';
+import { SIZES } from '@/shared/constants/dimensions';
+import React, { useCallback } from 'react';
+import type { CarouselRenderItem } from 'react-native-reanimated-carousel';
 
-export default class HomeScreen extends Component {
-  render() {
-    return (
-      <Container isScroll={true}>
-        <CardHorizantalFilter />
-        <RN.View fd={'row'} g={15} jc={'space-between'}>
-          <Card isPremium={true} />
-          <Card />
-        </RN.View>
-        <RN.View fd={'row'} g={15} jc={'space-between'}>
-          <Card isPremium={true} />
-          <Card />
-        </RN.View>
-        <RN.View fd={'row'} g={15} jc={'space-between'}>
-          <Card isPremium={true} />
-          <Card />
-        </RN.View>
-      </Container>
-    );
-  }
+const data = [
+  {
+    id: 1,
+    source: MockUserImagePng,
+    name: 'Jujutsu Kaisen',
+  },
+  {
+    id: 2,
+    source: MockUserImagePng,
+    name: 'Solo Spdsa',
+  },
+  {
+    id: 3,
+    source: MockUserImagePng,
+    name: 'Something',
+  },
+];
+
+const sizes = {
+  width: SIZES.width * 1,
+  height: SIZES.width * 1 * 0.8,
+};
+export default function HomeScreen() {
+  const renderItem: CarouselRenderItem<(typeof data)[0]> = useCallback(
+    ({ item: card }) => (
+      <RN.View>
+        <RN.Image
+          source={MockUserImagePng}
+          style={styles.movieImage}
+          contentFit={'contain'}
+        />
+      </RN.View>
+    ),
+    [],
+  );
+  return (
+    <Container isScroll={true}>
+      <Carousel
+        data={data}
+        renderItem={renderItem}
+        width={SIZES.width}
+        height={SIZES.width * 0.8}
+      />
+      <CardHorizantalFilter />
+      <RN.View fd={'row'} g={15} jc={'space-between'}>
+        <Card isPremium={true} />
+        <Card />
+      </RN.View>
+      <RN.View fd={'row'} g={15} jc={'space-between'}>
+        <Card isPremium={true} />
+        <Card />
+      </RN.View>
+      <RN.View fd={'row'} g={15} jc={'space-between'}>
+        <Card isPremium={true} />
+        <Card />
+      </RN.View>
+    </Container>
+  );
 }
+
+const styles = RN.StyleSheet.create({
+  movieImage: {
+    ...sizes,
+    borderRadius: 11,
+    overflow: 'hidden',
+    width: sizes.width * 0.9,
+  },
+});
---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,173 @@
+import { COLORS, addAlpha } from '@/shared/constants/colors';
+import React, { FC, ReactNode } from 'react';
+import Animated, {
+  Extrapolation,
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import type { CarouselRenderItem } from 'react-native-reanimated-carousel';
+import RNCarousel from 'react-native-reanimated-carousel';
+import RN from '../RN';
+
+type Props<T> = {
+  data: T[];
+  renderItem: CarouselRenderItem<T>;
+  loop?: boolean;
+  autoPlay?: boolean;
+  onSnapToItem?(index: number): void;
+  width: number;
+  height: number;
+  dotColor?: string;
+  fistImage?: ReactNode;
+};
+
+export default function Carousel<T>({
+  data = [],
+  renderItem,
+  loop = true,
+  autoPlay = true,
+  onSnapToItem,
+  width,
+  height,
+  dotColor,
+  fistImage,
+}: Props<T>) {
+  const progressValue = useSharedValue<number>(0);
+
+  return (
+    <>
+      {data.length > 1 ? (
+        <RNCarousel
+          vertical={false}
+          width={width}
+          height={height}
+          style={{
+            width: width,
+          }}
+          loop={loop}
+          pagingEnabled={true}
+          snapEnabled={true}
+          autoPlay={autoPlay}
+          autoPlayInterval={1500}
+          onSnapToItem={onSnapToItem}
+          onProgressChange={(_, absoluteProgress) =>
+            (progressValue.value = absoluteProgress)
+          }
+          mode={'parallax'}
+          modeConfig={{
+            parallaxScrollingScale: 0.95,
+            parallaxScrollingOffset: 150,
+            parallaxAdjacentItemScale: 0.8,
+          }}
+          data={data}
+          renderItem={renderItem}
+        />
+      ) : (
+        fistImage
+      )}
+
+      {!!progressValue && (
+        <RN.View style={styles.dots}>
+          {data.map((_, index) => (
+            <PaginationItem
+              backgroundColor={dotColor}
+              animValue={progressValue}
+              index={index}
+              key={index}
+              isRotate={false}
+              length={data.length}
+            />
+          ))}
+        </RN.View>
+      )}
+    </>
+  );
+}
+
+interface PaginationType {
+  index: number;
+  backgroundColor?: string;
+  length: number;
+  animValue: SharedValue<number>;
+  isRotate?: boolean;
+}
+
+const DOT_WIDTH = 10;
+
+const PaginationItem: FC<PaginationType> = (props) => {
+  const { animValue, index, length, isRotate } = props;
+
+  const animStyle = useAnimatedStyle(() => {
+    let inputRange = [index - 1, index, index + 1];
+    let outputRange = [-DOT_WIDTH, 0, DOT_WIDTH];
+
+    if (index === 0 && animValue?.value > length - 1) {
+      inputRange = [length - 1, length, length + 1];
+      outputRange = [-DOT_WIDTH, 0, DOT_WIDTH];
+    }
+
+    return {
+      transform: [
+        {
+          translateX: interpolate(
+            animValue?.value,
+            inputRange,
+            outputRange,
+            Extrapolation.CLAMP,
+          ),
+        },
+      ],
+    };
+  }, [animValue, index, length]);
+  return (
+    <RN.View
+      style={[
+        styles.dot,
+        !!props.backgroundColor && {
+          backgroundColor: addAlpha(props.backgroundColor, 0.5),
+        },
+        {
+          transform: [
+            {
+              rotateZ: isRotate ? '90deg' : '0deg',
+            },
+          ],
+        },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.animatedDot,
+          !!props.backgroundColor && {
+            backgroundColor: props.backgroundColor,
+          },
+          animStyle,
+        ]}
+      />
+    </RN.View>
+  );
+};
+
+const styles = RN.StyleSheet.create({
+  dots: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingTop: 20,
+    gap: 8,
+  },
+  dot: {
+    width: 10,
+    aspectRatio: 1,
+    borderRadius: 50,
+    backgroundColor: addAlpha(COLORS.white, 0.5),
+    overflow: 'hidden',
+  },
+  animatedDot: {
+    borderRadius: 50,
+    backgroundColor: COLORS.white,
+    flex: 1,
+  },
+});
