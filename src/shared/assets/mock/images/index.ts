a0bf7a5ebbe29b6f58a5d6827fec0423501c5d27
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2024-07-19 05:41:49 +0500

    refactoring

--- src/shared/assets/mock/images/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MockUserImagePng } from './movie.png';
--- src/shared/constants/platform.ts
+++ src/shared/constants/platform.ts
@@ -1,5 +1,5 @@
 import RN from '@/components/RN';
 
+export const isWeb = RN.Platform.OS === 'web';
 export const isAndroid = RN.Platform.OS === 'android';
 export const isIOS = RN.Platform.OS === 'ios';
-export const isWeb = RN.Platform.OS === 'web';
