079979d65672b1c82f8f0c8b2d45355f99ff04ad
Abdurakhmon <83976512+AbdurakhmonZiyodov@users.noreply.github.com>
2024-09-25 11:24:45 +0500

    Update index.ts
    refactoring config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,10 @@
-export const BASE_URL = 'https://api.bananatv.uz/api';
+export const BASE_URL = '';
 export const VIDEO_BASE_URL = `${BASE_URL}/video-stream/watch`;
 
 const GOOGLE = {
-  android:
-    '622012536210-t8vlj88apopk6tfkeumum8grpamj1t4j.apps.googleusercontent.com',
-  ios: '622012536210-95iakmt1ncea65ourkk96ofbgef738gb.apps.googleusercontent.com',
-  web: '622012536210-7qjb208n14o4qvk6mojoq31gf7014p4c.apps.googleusercontent.com',
+  android:'',
+  ios: '',
+  web: '',
 };
 
 export default {
